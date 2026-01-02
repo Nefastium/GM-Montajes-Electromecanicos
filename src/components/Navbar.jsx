bc37fe7f5c92c535e94fe3fd40395b8ac85c6acb
Nefastium <marinkovichezequiel@gmail.com>
2026-01-01 21:46:34 -0300

    Add mobile hamburger menu to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
+
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <h1 className="logo">GM Montajes</h1>
-      <ul className="nav-links">
-        <li>Inicio</li>
-        <li>Servicios</li>
-        <li>Nosotros</li>
-        <li>Contacto</li>
+
+      {/* Botón hamburguesa */}
+      <div
+        className="hamburger"
+        onClick={() => setOpen(!open)}
+      >
+        ☰
+      </div>
+
+      {/* Menú */}
+      <ul className={`nav-links ${open ? "open" : ""}`}>
+        <li onClick={() => setOpen(false)}>Inicio</li>
+        <li onClick={() => setOpen(false)}>Servicios</li>
+        <li onClick={() => setOpen(false)}>Nosotros</li>
+        <li onClick={() => setOpen(false)}>Contacto</li>
       </ul>
     </nav>
   );
