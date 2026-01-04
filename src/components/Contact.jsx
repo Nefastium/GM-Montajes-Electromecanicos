0e5b1627916238199423cb31660cfb2256d5a3c4
Nefastium <marinkovichezequiel@gmail.com>
2026-01-04 18:42:51 -0300

    Fixing.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ function Contact() {
   return (
     <section className="contact" id="contacto">
       <h2>Contacto</h2>
-      <p>📞 Teléfono: +54 9 1 5831-4455</p>
+      <p>📞 Teléfono: +54 9 11 5831-4455</p>
       <p>📧 Email: contacto@gmmontajes.com</p>
       <button>WhatsApp</button>
     </section>
